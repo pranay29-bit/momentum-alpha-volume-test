43a8666575dcb3404b4385bf2d17dc7cfbf73a95
pranay29-bit <64679614+pranay29-bit@users.noreply.github.com>
2026-06-15 13:47:51 +0530

    Update firebase.js

--- docs/js/firebase.js
+++ docs/js/firebase.js
@@ -14,12 +14,13 @@ signInWithPopup
 from "https://www.gstatic.com/firebasejs/11.9.0/firebase-auth.js";
 
 const firebaseConfig = {
-apiKey: "YOUR_API_KEY",
-authDomain: "YOUR_PROJECT.firebaseapp.com",
-projectId: "YOUR_PROJECT",
-storageBucket: "YOUR_PROJECT.appspot.com",
-messagingSenderId: "XXXX",
-appId: "XXXX"
+apiKey: "AIzaSyAEiHgA339Dtry7r5mL2zo4bXU_N9cLyjA",
+authDomain: "momentum-alpha-volume-test.firebaseapp.com",
+projectId: "momentum-alpha-volume-test",
+storageBucket: "momentum-alpha-volume-test.firebasestorage.app",
+messagingSenderId: "391686611939",
+appId: "1:391686611939:web:a5b1fac37c80a43d97f6d6",
+measurementId: "G-MCHKXZRP0E"
 };
 
 export const app =

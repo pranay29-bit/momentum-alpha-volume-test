d84860d98453b012d67cd2dfa1240034ac137cde
pranay29-bit <64679614+pranay29-bit@users.noreply.github.com>
2026-06-15 12:46:50 +0530

    Update portfolio.js

--- docs/js/portfolio.js
+++ docs/js/portfolio.js
@@ -1,1 +1,93 @@
+import {
+db,
+auth,
+login
+}
+from "./firebase.js";
 
+import {
+collection,
+addDoc,
+getDocs,
+deleteDoc,
+doc
+}
+from "https://www.gstatic.com/firebasejs/11.9.0/firebase-firestore.js";
+
+const addBtn =
+document.getElementById("addBtn");
+
+addBtn.onclick =
+async () => {
+
+if(!auth.currentUser){
+
+await login();
+
+}
+
+const portfolio =
+Number(
+document.getElementById(
+"portfolioSize"
+).value
+);
+
+const riskPct =
+Number(
+document.getElementById(
+"riskPct"
+).value
+);
+
+const entry =
+Number(
+document.getElementById(
+"entry"
+).value
+);
+
+const stop =
+Number(
+document.getElementById(
+"stop"
+).value
+);
+
+const symbol =
+document.getElementById(
+"symbol"
+).value;
+
+const riskAmount =
+portfolio *
+riskPct /
+100;
+
+const qty =
+Math.floor(
+riskAmount /
+(entry-stop)
+);
+
+await addDoc(
+
+collection(
+db,
+"users",
+auth.currentUser.uid,
+"positions"
+),
+
+{
+symbol,
+entry,
+stop,
+qty
+}
+
+);
+
+loadPositions();
+
+};
